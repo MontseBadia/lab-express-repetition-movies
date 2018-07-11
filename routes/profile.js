338812129170c8b67a3c04c1228d897ed2a652ee
Montse Badia <mbadia_11@hotmail.com>
2018-07-11 23:59:16 +0200

    improved tracking of favs

--- routes/profile.js
+++ routes/profile.js
@@ -4,13 +4,22 @@ const express = require('express');
 const router = express.Router();
 
 const User = require('../models/users');
+const Movie = require('../models/movie');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   const userData = req.session.currentUser;
   User.findById(userData._id)
     .then((user) => {
-      res.render('profile', {user: user});
+      let moviesTitles = [];
+      user.favorites.forEach(element => {
+        Movie.findById(element)
+          .then((movie) => {
+            moviesTitles.push(movie.title);
+          })
+          .catch(next);
+      });
+      res.render('profile', {user: user, moviesTitles: moviesTitles}); // how to put this inside forEach?
     })
     .catch(next);
   // console.log(userData);
@@ -24,21 +33,17 @@ router.post('/favorites', (req, res, next) => {
       const isFav = user.favorites.find(item => item.toString() === favMovieId);
       // console.log(isFav);
       if (!isFav) {
-        User.findByIdAndUpdate(userData._id, {$push: {favorites: favMovieId}})
-          .then((updatedUser) => {
+        return User.findByIdAndUpdate(userData._id, {$push: {favorites: favMovieId}})
+          .then(() => {
           // req.session.currentUser = updatedUser; // Does this work?
-          // console.log('isFav in is not present :' + updatedUser);
             res.redirect(`/movies/${favMovieId}`); // without :
-          })
-          .catch(next);
+          });
       } else {
-        User.findByIdAndUpdate(userData._id, {$pull: {favorites: favMovieId}})
-          .then((updatedUser) => {
+        return User.findByIdAndUpdate(userData._id, {$pull: {favorites: favMovieId}})
+          .then(() => {
           // req.session.currentUser = updatedUser; // Does this work?
-          // console.log('isFav in is present :' + updatedUser);
             res.redirect(`/movies/${favMovieId}`);
-          })
-          .catch(next);
+          });
       }
     });
 });
